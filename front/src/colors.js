5c3e979cdbb76211ebb2d78ba625aebc17afe573
Victor ELY <vely@juniorisep.com>
2017-06-15 13:07:36 +0200

    Prepare project

---
+++ front/src/colors.js
@@ -0,0 +1,11 @@
+// @flow
+
+const colors = {
+  ISEP_PRIMARY: '#005580',
+  ISEP_PRIMARY_LIGHTER: 'rgba(0, 85, 128, 0.8)',
+  ISEP_SECONDARY: '#ff9800',
+  ISEP_SECONDARY_LIGHTER: 'rgb(255, 190, 0)',
+  ISEP_TERTIARY: '#ffffff',
+};
+
+export default colors;

edd6d37ceaabeec45e45efa2b9a8932cba9ec900
Victor Ely <vely@juniorisep.com>
2017-07-11 14:54:46 +0200

    Add Grid.scss. Prepare Route

--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+import { MuiThemeProvider } from 'material-ui/styles';
+
+import Login from './pages/login';
+
+const App = () => (
+  <MuiThemeProvider>
+    <Router>
+      <Switch>
+        <Redirect path="/" exact to="/login" />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
+  </MuiThemeProvider>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ front/src/pages/login/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Connexion</h1>
+    </div>
+  );
+}

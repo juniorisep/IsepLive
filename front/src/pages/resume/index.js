6fca4c4b77f67c0a7708c03e2c06b04d66ddf17f
Victor Ely <vely@juniorisep.com>
2017-07-20 15:43:19 +0200

    Improve resume

--- front/src/pages/resume/index.js
+++ front/src/pages/resume/index.js
@@ -2,6 +2,8 @@
 
 import React, { Component } from 'react';
 
+import styled from 'styled-components';
+
 import { Flex, Box } from 'grid-styled';
 
 import {
@@ -11,6 +13,20 @@ import {
   Filler,
   Banner,
 } from '../../components/common';
+
+const Person = (props) => {
+  const PersonStyle = styled.div`
+    box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+    > img {
+      width: 100%;
+    }
+  `;
+  return (
+    <PersonStyle>
+      <img src={props.url} alt="person-image"/>
+    </PersonStyle>
+  )
+}
 
 class Resume extends Component {
   render() {
@@ -27,7 +43,25 @@ class Resume extends Component {
           </FluidContent>
         </Header>
         <FluidContent>
-
+          <Flex wrap>
+            <Box px={2} py={1} width={1/4}>
+              <Person
+                url="https://numeris-isep.fr/img/team//amalric.resized.jpg"
+              />
+          	</Box>
+            <Box px={2} py={1} width={3/4}>
+              Informations
+            </Box>
+            <Box px={2} py={1} width={1/1}>
+              Citation
+            </Box>
+            <Box px={2} py={1} width={1/1}>
+              Association
+            </Box>
+            <Box px={2} py={1} width={1/1}>
+              Publications
+            </Box>
+          </Flex>
         </FluidContent>
       </div>
     );

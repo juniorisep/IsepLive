fecdcfa9433974cf8c4acc6c99422ac6465cd6ad
GuillaumeCa <guillaume.carre@isep.fr>
2017-07-20 19:18:41 +0200

    fix profile image

--- front/src/pages/resume/index.js
+++ front/src/pages/resume/index.js
@@ -28,8 +28,8 @@ import {
 
 const Person = (props) => {
   const PersonStyle = styled.div`
-    box-shadow: 10px 10px 5px rgba(0,0,0,0.1);
     > img {
+      box-shadow: 0px 5px 15px rgba(0,0,0,0.1);
       width: 100%;
     }
   `;

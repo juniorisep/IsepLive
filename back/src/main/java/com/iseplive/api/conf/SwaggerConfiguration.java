9530b2d31447d9a189107738883e43ccc5e89140
GuillaumeCa <guillaume.carre@isep.fr>
2017-08-01 00:06:00 +0200

    add swagger

---
+++ back/src/main/java/com/iseplive/api/conf/SwaggerConfiguration.java
@@ -0,0 +1,33 @@
+package com.iseplive.api.conf;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Guillaume on 31/07/2017.
+ * back
+ */
+@Configuration
+@EnableSwagger2
+@ComponentScan("com.iseplive.api")
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .select()
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .build();
+    }
+}

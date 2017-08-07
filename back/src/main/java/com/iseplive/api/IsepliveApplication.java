2e2c17296e778c006cfb6b84bbebcdcc3ccd091e
Victor Ely <vely@juniorisep.com>
2017-08-07 11:40:42 -0500

    DatabaseSeeder

--- back/src/main/java/com/iseplive/api/IsepliveApplication.java
+++ back/src/main/java/com/iseplive/api/IsepliveApplication.java
@@ -2,12 +2,18 @@ package com.iseplive.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class IsepliveApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(IsepliveApplication.class, args);
+		ConfigurableApplicationContext ac = SpringApplication.run(IsepliveApplication.class, args);
+		DatabaseSeeder tdbs = ac.getBeanFactory().createBean(DatabaseSeeder.class);
+		tdbs.seedDatabase();
 	}
 }

2e6d71699b40862411164f2337cd8de357381968
Victor Ely <vely@juniorisep.com>
2017-08-13 21:22:43 -0500

    Prepare contact

--- front/src/pages/contact/index.js
+++ front/src/pages/contact/index.js
@@ -4,6 +4,12 @@ import React, {Component} from 'react';
 
 import {Banner, Filler, FluidContent, Header, SearchBar} from 'components/common';
 
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+
+// API_KEY_GOOGLE: AIzaSyA2PjbZfTlgzBMgpuPUuJ0_9kFf-_nOxdM
+// Google_Maps: https://github.com/tomchentw/react-google-maps
+
 class Contact extends Component {
   render() {
     return (
@@ -18,7 +24,42 @@ class Contact extends Component {
             <SearchBar placeholder="Rechercher" />
           </FluidContent>
         </Header>
-        <FluidContent></FluidContent>
+        <FluidContent>
+          <TextField
+            id="Nom"
+            label="Nom"
+            InputProps={{ placeholder: 'Nom' }}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            id="Adresse email"
+            label="Adresse email"
+            InputProps={{ placeholder: 'Adresse email' }}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            id="Téléphone"
+            label="Téléphone"
+            InputProps={{ placeholder: 'Téléphone' }}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            id="Message"
+            label="Message"
+            InputProps={{ placeholder: 'Message' }}
+            fullWidth
+            multiline
+            rows="4"
+            margin="normal"
+          />
+          <p>Adresse</p>
+          <p>Mail</p>
+          <Button raised color="accent">Envoyer</Button>
+          <h1>Mettre google maps ici ou avant le formulaire</h1> {/* TODO Mettre google maps ici ou avant le formulaire*/}
+        </FluidContent>
       </div>
     );
   };

d45ab543ef23f26fa49704ed85780f884bdefd23
Victor Ely <vely@juniorisep.com>
2017-10-09 14:12:35 +0200

    Clean code

--- front/src/pages/home/view.js
+++ front/src/pages/home/view.js
@@ -13,11 +13,6 @@ import Auth from 'components/Auth/AuthComponent';
 import PublishBoxView from './publishBox';
 import Loader from 'components/Loader';
 
-/* const IconMenu = styled.div`
-  display: flex;
-  justify-content: space-between;
-`; */
-
 const FakeIcon = styled.div`
   margin: 0 auto;
   width: 100px;
@@ -26,21 +21,6 @@ const FakeIcon = styled.div`
   margin-bottom: 10px;
   border-radius: 100px;
 `;
-
-/*const Icon = styled.img`
-  display: block;
-  margin: 0 auto;
-  width: 100px;
-  height: 100px;
-  margin-bottom: 10px;
-`;
-
-const IconName = styled.h3`
-  text-align: center;
-  margin: 0;
-  margin-bottom: 10px;
-  color: ${props => props.theme.accent};
-`;*/
 
 const Background = styled.div`
   background: url(/img/background.jpg);
@@ -61,15 +41,9 @@ const Overlay = styled.div`
   opacity: 0.6;
 `;
 
-/*const About = styled.div`
-  margin-bottom: 50px;
-`;*/
-
 const PostSection = styled.div`
   margin: 30px 0;
 `;
-
-const SectionTitle = Title.withComponent('h2');
 
 const Center = styled.div`
   text-align: center;
@@ -88,38 +62,6 @@ export default function Home(props) {
           <SearchBar placeholder="Rechercher" />
         </FluidContent>
       </Header>
-      {/*      <FluidContent>
-        <Flex wrap justify="space-between">
-          <Box w={[
-        1, 1 / 2,
-        1 / 4
-          ]}>
-        <Icon src="/img/svg/Video.svg" />
-        <IconName>Videos</IconName>
-          </Box>
-          <Box w={[
-        1, 1 / 2,
-        1 / 4
-          ]}>
-        <Icon src="/img/svg/Photo.svg" />
-        <IconName>Photos</IconName>
-          </Box>
-          <Box w={[
-        1, 1 / 2,
-        1 / 4
-          ]}>
-        <Icon src="/img/svg/Gazette.svg" />
-        <IconName>Gazettes</IconName>
-          </Box>
-          <Box w={[
-        1, 1 / 2,
-        1 / 4
-          ]}>
-        <Icon src="/img/svg/Evenement.svg" />
-        <IconName>Evenements</IconName>
-          </Box>
-        </Flex>
-      </FluidContent*/}
       <Background>
         <Overlay />
         <FluidContent>
@@ -128,7 +70,6 @@ export default function Home(props) {
             <Separator />
           </Auth>
           <PostSection>
-            {/*<SectionTitle fontSize={2} framed>A LA UNE...</SectionTitle>*/}
             <Loader loading={props.isLoading}>
               <div>
                 <PostListView posts={props.posts} refreshPosts={props.refreshPosts} />
@@ -143,45 +84,6 @@ export default function Home(props) {
           </PostSection>
         </FluidContent>
       </Background>
-      {/* TODO Voir avec ISEP Live
-      <FluidContent>
-        <About>
-          <SectionTitle fontSize={2} framed>L'EQUIPE</SectionTitle>
-          <Flex wrap>
-            <Box w={[ 1, 1 / 2 ]} p={2}>
-              <img src="/img/background.jpg" alt="" width="100%" />
-            </Box>
-            <Box w={[ 1, 1 / 2 ]} p={2}>
-              <Text>
-                Wei 2K18
-              </Text>
-              <Text color="#3a40d7">
-                WEI Ingé 2015 - JT -
-              </Text>
-            </Box>
-          </Flex>
-        </About>
-        <Separator  />
-          <SectionTitle fontSize={2} framed>SUIVEZ-NOUS</SectionTitle>
-          <Flex wrap justify="space-between">
-          <Box w={[1, 1/2, 1/4]}>
-            <FakeIcon  />
-            <IconName>Facebook</IconName>
-          </Box>
-          <Box w={[1, 1/2, 1/4]}>
-            <FakeIcon  />
-            <IconName>Twitter</IconName>
-          </Box>
-          <Box w={[1, 1/2, 1/4]}>
-            <FakeIcon  />
-            <IconName>Instagram</IconName>
-          </Box>
-          <Box w={[1, 1/2, 1/4]}>
-            <FakeIcon  />
-            <IconName>Snapchat</IconName>
-          </Box>
-        </Flex>
-      </FluidContent> */}
     </div>
   );
 };

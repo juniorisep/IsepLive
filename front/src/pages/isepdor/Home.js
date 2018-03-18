8bf9bba3279bd549590b74ec8a5543fea9532013
GuillaumeCa <guillaume.carre@isep.fr>
2018-03-18 22:43:12 +0100

    add isep d'or button

--- front/src/pages/isepdor/Home.js
+++ front/src/pages/isepdor/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import {
-  Paper,
-  Title,
-  Text,
-  FluidContent,
-} from '../../components/common';
+import { Paper, Title, Text, FluidContent } from '../../components/common';
 
 import Button from 'material-ui/Button';
 
+import StarIcon from 'material-ui-icons/Star';
 const btnStyle = {
   margin: 10,
+};
+
+const pollBtnStyle = {
+  position: 'fixed',
+  right: 0,
+  bottom: 0,
+  margin: 20,
 };
 
 export default class IsepDorHome extends React.Component {
@@ -18,13 +21,20 @@ export default class IsepDorHome extends React.Component {
     return (
       <Paper p="2em">
         <Title>ISEP d'Or</Title>
-        <Text>
-          Les résultats de la session de 2018 sont disponibles !
-          </Text>
+        <Text>Les résultats de la session de 2018 sont disponibles !</Text>
         <div>
-          <Button style={btnStyle} raised color="primary">Résultats</Button>
-          <Button style={btnStyle} color="primary">Voir tout</Button>
+          <Button style={btnStyle} raised color="primary">
+            Résultats
+          </Button>
+          <Button style={btnStyle} color="primary">
+            Voir tout
+          </Button>
         </div>
+        {/* <ToolTip title="ISEP d'Or" placement="left"> */}
+        <Button fab color="secondary" style={pollBtnStyle}>
+          <StarIcon />
+        </Button>
+        {/* </ToolTip> */}
       </Paper>
     );
   }
